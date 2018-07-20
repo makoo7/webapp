93935ddf42ef7ffe9f4f6f8bbc2005ab84dc0f5f
mak <makoo7.prajapati@gmail.com>
2018-07-20 10:05:07 +0530

    mongodb connection to nodejs

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
-
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect('mongodb://<username>:<password>@<connectionstring>',{useNewUrlParser: true});
+
+mongoose.connection.once('open',()=>{
+    console.log('connection to mongoose successfully');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
